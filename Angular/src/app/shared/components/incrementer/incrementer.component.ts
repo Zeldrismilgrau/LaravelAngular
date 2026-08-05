b91ef4d8a604e6b9d1447660c3277ed91f5ef33b
Zeldrismilgrau <Gabrielbrfernandes000@gmail.com>
2026-08-05 11:52:27 -0300

    IAIAIAIIAIAIA]

---
+++ Angular/src/app/shared/components/incrementer/incrementer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementer',
+  templateUrl: './incrementer.component.html',
+  styleUrls: ['./incrementer.component.scss'],
+})
+export class IncrementerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
